1d2baa2c49f11e8f9fd5900b3ed71701bc815f0e
aegisss <113986232+aegisss@users.noreply.github.com>
2023-02-09 16:12:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+namespace Piano
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Нажмите F1, F2, F3 и т.д. для переключения между октавами.");
+
+            int[] firstOctave = new int[] { 200, 300, 400 };
+            int[] secondOctave = new int[] { 800, 900, 1000 };
+            int[] thirdOctave = new int[] { 1600, 1700, 1800 };
+            int[] currentOctave = firstOctave;
+
+            while (true)
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.F1:
+                        currentOctave = firstOctave;
+                        break;
+                    case ConsoleKey.F2:
+                        currentOctave = secondOctave;
+                        break;
+                    case ConsoleKey.F3:
+                        currentOctave = thirdOctave;
+                        break;
+                    default:
+                        int index = Array.IndexOf(currentOctave, keyInfo.KeyChar);
+                        if (index >= 0)
+                        {
+                            PlaySound(currentOctave[index]);
+                        }
+                        break;
+                }
+            }
+        }
+
+        static void PlaySound(int frequency)
+        {
+            Console.Beep(frequency, 500);
+        }
+    }
+}
